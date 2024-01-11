8ea3b56eb4d1e40a67232cac2cdeb5ee74c107e4
sillonjeu <pius0316@naver.com>
2024-01-12 04:52:04 +0900

    feat: 서버 만들기, 친구 추가 틀 구성

--- Hansori/lib/main.dart
+++ Hansori/lib/main.dart
@@ -1,125 +1,180 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Discord Server Creation Example',
+      home: CreateServerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CreateServerPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CreateServerPageState createState() => _CreateServerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CreateServerPageState extends State<CreateServerPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _serverNameController = TextEditingController();
+  File? _image;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
+  void _createServer() {
+    if (_formKey.currentState!.validate()) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => FriendsListPage(serverName: _serverNameController.text),
+        ),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _serverNameController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Create a Server'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: [
+            Center(
+              child: InkWell(
+                onTap: _pickImage,
+                child: ClipOval(
+                  child: _image != null
+                      ? Image.file(_image!, width: 100, height: 100, fit: BoxFit.cover)
+                      : Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                    child: Icon(Icons.camera_alt, color: Colors.white),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20),
+            TextFormField(
+              controller: _serverNameController,
+              decoration: InputDecoration(
+                labelText: 'Server Name',
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value!.isEmpty) {
+                  return 'Please enter a server name';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _createServer,
+              child: Text('Create Server'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class FriendsListPage extends StatefulWidget {
+  final String serverName;
+
+  FriendsListPage({required this.serverName});
+
+  @override
+  _FriendsListPageState createState() => _FriendsListPageState();
+}
+
+class _FriendsListPageState extends State<FriendsListPage> {
+  final List<String> friends = [
+    'Friend 1',
+    'Friend 2',
+    'Friend 3',
+    'Friend 4',
+    // api로 변경 필요
+  ];
+
+  Set<String> _invitedFriends = {};
+
+  void _inviteFriend(String friendName) {
+    setState(() {
+      _invitedFriends.add(friendName);
+    });
+  }
+
+  void _skipOrComplete() {
+    if (_invitedFriends.isNotEmpty) {
+      // 초대 완료 로직
+    } else {
+      // 건너뛰기 로직
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Invite Friends to ${widget.serverName}'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: friends.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  contentPadding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                  title: Text(
+                    friends[index],
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  trailing: ElevatedButton(
+                    onPressed: () => _inviteFriend(friends[index]),
+                    child: Text('Invite'),
+                  ),
+                );
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: _skipOrComplete,
+                child: Text(_invitedFriends.isNotEmpty ? '완료' : '건너뛰기'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
